2e44a7f5d9e99480c1d98a86d128faf9989fc390
Lukasz Zywluk <lukasz.zywluk@gmail.com>
2022-08-10 18:02:16 -0400

    Upgrade to Astro 1.0.1

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,40 +1,39 @@
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-	// Comment out "renderers: []" to enable Astro's default component support.
-	//renderers: [],
-	vite: {
-		plugins: [],
-	},
-	buildOptions: {
-		site: "https://byteofdev.com",
-	},
-	markdownOptions: {
-		render: [
-			"@astrojs/markdown-remark",
-			{
-				remarkPlugins: [
-					"remark-gfm",
-					"remark-smartypants",
-					"remark-prism",
-					"@fec/remark-a11y-emoji",
-					["remark-behead", { depth: 1 }],
-				],
-				rehypePlugins: [
-					"rehype-slug",
-					[
-						"rehype-autolink-headings",
-						{
-							content: {
-								type: "element",
-								tagName: "span",
-								properties: { className: ["headerLink"], ariaHidden: true },
-								children: [],
-							},
-							behavior: "append",
-						},
-					],
-				],
-			},
-		],
-	},
+import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import svelte from "@astrojs/svelte"; // @ts-check
+// https://astro.build/config
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig(
+/** @type {import('astro').AstroUserConfig} */
+{
+  // Comment out "renderers: []" to enable Astro's default component support.
+  //renderers: [],
+  vite: {
+    plugins: [svelte({})]
+  },
+  build: {},
+  site: "https://byteofdev.com",
+  markdown: {
+    // Can be 'shiki' (default), 'prism' or false to disable highlighting
+    syntaxHighlight: 'prism',
+    remarkPlugins: ["remark-gfm", "remark-smartypants", "remark-prism", "@fec/remark-a11y-emoji", ["remark-behead", {
+      depth: 1
+    }]],
+    rehypePlugins: ["rehype-slug", ["rehype-autolink-headings", {
+      content: {
+        type: "element",
+        tagName: "span",
+        properties: {
+          className: ["headerLink"],
+          ariaHidden: true
+        },
+        children: []
+      },
+      behavior: "append"
+    }]]
+  },
+  integrations: [mdx(), svelte(), tailwind()]
 });
--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -5,13 +5,13 @@
 
 <a class="rounded-md bg-base-100 p-4 shadow-md" href={item.url + "/"}>
 	<img
-		src={item.image.url}
+		src={item.frontmatter.image.url}
 		alt="Article header"
 		class="rounded-md w-full aspect-video bg-base-300"
 		loading="lazy"
 	/>
-	<h2 class="font-bold text-xl">{item.title}</h2>
-	<p>{item.description}</p>
-	<p>By {item.author}</p>
-	<p>{dayjs(item.published).format("MMMM D, YYYY")}</p>
+	<h2 class="font-bold text-xl">{item.frontmatter.title}</h2>
+	<p>{item.frontmatter.description}</p>
+	<p>By {item.frontmatter.author}</p>
+	<p>{dayjs(item.frontmatter.published).format("MMMM D, YYYY")}</p>
 </a>
